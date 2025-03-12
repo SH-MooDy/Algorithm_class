81b9a30562610f22a594519bd9865bee3d5d5ae2
SH-MooDy <shlee4167@naver.com>
2025-03-12 23:37:22 +0900

    first

---
+++ week2/prob1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int N, K, rank;
+  int arr[1000];
+
+  // 정수 입력
+  for (int i = 0; i < N; i++) {
+    cin >> arr[i];
+  }
+
+  return 0;
+}
