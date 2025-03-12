6222ab3f08867df608461862a40bacd749d665ed
SH-MooDy <shlee4167@naver.com>
2025-03-13 01:57:59 +0900

    prob1 complete

--- week2/prob1.cpp
+++ week2/prob1.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
-
 using namespace std;
 
-int main() {
-  int N, K, rank;
-  int arr[1000];
+int N, K;
+int arr[1000];
 
-  // 정수 입력
+int findRank(int idx, int rank) {
+  if (idx == N) {
+    return rank + 1;
+  }
+
+  if (arr[idx] < K) {
+    return findRank(idx + 1, rank + 1);
+  } else {
+    return findRank(idx + 1, rank);
+  }
+}
+
+int main() {
+  cin >> N;
   for (int i = 0; i < N; i++) {
     cin >> arr[i];
   }
+  cin >> K;
 
+  int rank = 0;
+
+  cout << findRank(0, rank) << endl;
   return 0;
 }
