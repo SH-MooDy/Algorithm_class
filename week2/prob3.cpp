35260d90b97711b5762a33bdbbbd3d77e22187ba
SH-MooDy <shlee4167@naver.com>
2025-03-14 13:13:21 +0900

    피곤해애

--- week2/prob3.cpp
+++ week2/prob3.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int n;
 int maze[16][16];
 
-int main() { int N, return 0; }
+void inputMaze() {
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cin >> maze[i][j];
+    }
+  }
+}
+int main() {
+  inputMaze();
+
+  return 0;
+}
