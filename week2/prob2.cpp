b8b8ce68f08daa31992298c98fff42bb9af728be
SH-MooDy <shlee4167@naver.com>
2025-03-14 13:03:49 +0900

    prob2 complete

--- week2/prob2.cpp
+++ week2/prob2.cpp
@@ -1,25 +1,41 @@
+#include <algorithm>
 #include <iostream>
 using namespace std;
 
 int arr[1000];
-int N, K;
 
-int nearest(int target, int begin, int end) {
-  int middle = (begin + 2) / 2;
+int binarySearch(int begin, int end, int K) {
+  if (begin >= end) {
+    return begin;
+  }
 
-  if (end - begin == 2) {
-    if () } else {
-    if (arr[middle] == target) {
-      return middle;
-    } else if (arr[middle] > target) {
-      return nearest(target, begin, middle - 1);
-    } else {
-      return nearest(target, middle + 1, end);
-    }
+  int middle = (begin + end) / 2;
+
+  if (arr[middle] < K) {
+    return binarySearch(middle + 1, end, K);
+  } else {
+    return binarySearch(begin, middle, K);
   }
 }
 
+int nearest(int N, int K) {
+  int idx = binarySearch(0, N, K);
+
+  if (idx == 0) {
+    return arr[0];
+  }
+  if (idx == N) {
+    return arr[N - 1];
+  }
+
+  if (abs(arr[idx] - K) < abs(arr[idx - 1] - K)) {
+    return arr[idx];
+  } else {
+    return arr[idx - 1];
+  }
+}
 int main() {
+  int N, K;
   cin >> N;
 
   for (int i = 0; i < N; i++) {
@@ -28,7 +44,7 @@ int main() {
 
   cin >> K;
 
-  cout << nearest(K, 0, N - 1) << endl;
+  cout << nearest(N, K) << endl;
   return 0;
 }
 
---
+++ week2/prob3.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int maze[16][16];
+
+int main() { int N, return 0; }
