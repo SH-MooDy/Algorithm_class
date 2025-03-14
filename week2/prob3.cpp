637f946547ad741c83fb0378368c0940042e14ff
SH-MooDy <shlee4167@naver.com>
2025-03-14 13:18:15 +0900

    수업듣기 싫어어

--- week2/prob3.cpp
+++ week2/prob3.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int n;
+int N, K;
 int maze[16][16];
 
 void inputMaze() {
-  cin >> n;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
       cin >> maze[i][j];
     }
   }
+  cin >> K;
 }
 int main() {
   inputMaze();
