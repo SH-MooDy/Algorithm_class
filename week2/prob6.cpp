e09183c293a7f6f0f2dc6f09bb217282cf7fed0a
SH-MooDy <shlee4167@naver.com>
2025-03-17 21:14:36 +0900

    prob6 modify

--- week2/prob6.cpp
+++ week2/prob6.cpp
@@ -3,15 +3,18 @@ using namespace std;
 
 int n;
 int arr[20];
+bool visited[20];
 
 bool findPath(int pos) {
-  if (pos == n - 1) {  // 수열의 끝에 도착
+  if (pos >= n - 1) {  // 수열의 끝에 도착
     return true;
   }
 
-  if (arr[pos] == 0) {  // 0에 도착 시 실패
+  if (arr[pos] == 0 || visited[pos]) {  // 0에 도착 or 이미 방문한 경우 실패
     return false;
   }
+
+  visited[pos] = true;  // 방문 처리
 
   // 해당 칸의 수 이하 만큼 이동 가능
   for (int dist = 1; dist <= arr[pos]; dist++) {
