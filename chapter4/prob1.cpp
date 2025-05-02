f1187975677156d60910d7acc289006a0b67a5bc
SH-MooDy <shlee4167@naver.com>
2025-05-02 14:02:29 +0900

    임시저장

--- chapter4/prob1.cpp
+++ chapter4/prob1.cpp
@@ -12,6 +12,16 @@ int main() {
   infile >> N;
 
   for (int i = 0; i < N; i++) {
+    int num, left_child, right_child;
+    infile >> num >> left_child >> right_child;
+
+    // 노드하나에 대한한 정보들을 node 벡터에 저장
+    node.push_back(num);
+    node.push_back(left_child);
+    node.push_back(right_child);
+
+    // 노드를 node_list 벡터에 저장
+    node_list.push_back();
   }
 
   return 0;
