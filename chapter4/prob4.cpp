28e83cc4f890c8044da714f31d0c593b95c9aa71
SH-MooDy <shlee4167@naver.com>
2025-05-11 17:32:57 +0900

    임시저장

--- chapter4/prob4.cpp
+++ chapter4/prob4.cpp
@@ -2,61 +2,90 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 // 한 사람에 대한 정보를 저장하는 클래스
 class Person {
  public:
-  string name, company, address, zip, phone, email;
+  string name, company, address, zipcode, phone, email;
 
   Person() = default;
   // 생성자
-  Person(string name, string company, string address, string zip, string phone,
-         string email)
+  Person(string name, string company, string address, string zipcode,
+         string phone, string email)
       : name(name),
         company(company),
         address(address),
-        zip(zip),
+        zipcode(zipcode),
         phone(phone),
         email(email) {}
 };
-
-vector<Person> people;
 
 // 이진 검색 트리의 노드를 나타내는 구조체
 struct Node {
   Person data;
   Node* parent;
-  Node* leftC;
-  Node* rightC;
+  Node* left;
+  Node* right;
 
   Node(Person& p) : data(p), parent(nullptr), left(nullptr), right(nullptr) {}
-}
+};
 
 class BST {
  private:
   Node* root;
 
-  void _printInorder(Node* node) const {
-    if (!node) return;
-    _printInorder(node->left);
+  // 한 사람 정보를 출력
+  void print_info(Node* node) {
     cout << node->data.name << endl;
-    cout << "\tCompany: " << node->data.company << endl;
-    cout << "\tAddress: " << node->data.address << endl;
-    cout << "\tZipcode: " << node->data.zip << endl cout
-         << "\tPhone: " << node->data.phone << endl;
-    cout << "\tEmail: " << node->data.email << endl;
-    _printInorder(node->right);
+    cout << "   Company: " << node->data.company << endl;
+    cout << "   Address: " << node->data.address << endl;
+    cout << "   Zipcode: " << node->data.zipcode << endl;
+    cout << "   Phone: " << node->data.phone << endl;
+    cout << "   Email: " << node->data.email << endl;
+  }
+
+  // 트리를 중순위로 순회하면서 출력
+  void printInorder(Node* node) {
+    if (!node) return;
+    printInorder(node->left);
+    print_info(node);
+    printInorder(node->right);
+  }
+
+  // 중순위로 일치하는 이름을 탐색
+  Node* findByName(Node* node, string& find_name) {
+    if (!node) return nullptr;
+
+    if (Node* found = findByName(node->left, find_name)) {
+      return found;
+    }
+    if (node->data.name == find_name) {
+      return node;
+    }
+
+    return findByName(node->right, find_name);
+  }
+
+  bool traceAndPrint(Node* node, string& name) {
+    if (!node) return false;
+    if (node->data.name == name || traceAndPrint(node->left, name) ||
+        traceAndPrint(node->right, name)) {
+      print_info(node);
+      return true;
+    }
+    return false;
   }
 
  public:
-  BST() : root(nullptr) {}  // 생성자, 루트를 pullptr로 초기화
+  BST() : root(nullptr) {}  // 생성자, 루트를 mullptr로 초기화
 
   // 출력
-  void print() const { _printInorder(root); }
+  void printList() { printInorder(root); }
 
   // 삽입
-  void insert(const Person& p) {
+  void insert(Person& p) {
     Node* newNode = new Node(p);
     if (!root) {
       root = newNode;
@@ -77,48 +106,114 @@ class BST {
     else
       parent->right = newNode;
   }
+
+  // 검색
+  void findAndPrint(string& name) {
+    Node* node = findByName(root, name);
+    if (node == nullptr) {
+      cout << name << " does not exist" << endl;
+    }
+    print_info(node);
+  }
+
+  Node* find(string& name) { return findByName(root, name); }
+
+  // 추가
+  void add(string& name) {
+    Person p;
+    p.name = name;
+    cout << "Company? ";
+    getline(cin, p.company);
+    cout << "Address? ";
+    getline(cin, p.address);
+    cout << "Zipcode? ";
+    getline(cin, p.zipcode);
+    cout << "phone? ";
+    getline(cin, p.phone);
+    cout << "Email? ";
+    getline(cin, p.email);
+
+    insert(p);
+    cout << name << " was successfully added." << endl;
+  }
+
+  // 추적
+  void trace(string& name) {
+    if (!traceAndPrint(root, name)) {
+      cout << name << "";
+    }
+  }
 };
 
+// 문자열에서 큰 따옴표 제거
+string extractQuoted(string s) {
+  size_t start = s.find('"');
+  if (start == string::npos) return "";
+
+  size_t end = s.find('"', start + 1);
+  if (end == string::npos) return "";
+
+  return s.substr(start + 1, end - start - 1);
+}
+
 // 파일 읽기
-void read_file(string fileName) {
+void readFile(string fileName, BST& bst) {
   ifstream infile(fileName);
   string line;
-
   getline(infile, line);  // 첫줄 무시
 
   while (getline(infile, line)) {
     stringstream ss(line);
-    string name, company, address, zip, phone, email;
+    Person p;
+    string tmp;
+    getline(ss, p.name, '\t');
+    getline(ss, tmp, '\t');
+    {
+      string inner = extractQuoted(tmp);
+      p.company = inner.empty() ? tmp : inner;
+    }
+    getline(ss, tmp, '\t');
+    {
+      string inner = extractQuoted(tmp);
+      p.address = inner.empty() ? tmp : inner;
+    }
 
-    getline(ss, name, '\t');
-    getline(ss, company, '\t');
-    getline(ss, address, '\t');
-    getline(ss, zip, '\t');
-    getline(ss, phone, '\t');
-    getline(ss, email, '\t');
-
-    Person P(name, company, address, zip, phone, email);
-    people.push_back(P);
+    getline(ss, p.zipcode, '\t');
+    getline(ss, p.phone, '\t');
+    getline(ss, p.email, '\t');
+    bst.insert(p);
   }
+  infile.close();
 }
 
+void writeFile(string fileName, BST& bst) { ofstream outfile(fileName); }
+
 int main() {
-  string input;
+  BST address_list;
+  string input, command, arg;
   while (true) {
     cout << "$ ";
     getline(cin, input);
 
-    string command, option;
     stringstream ss(input);
-    ss >> command >> option;
+    ss >> command >> arg;
 
     if (command == "exit") {
       break;
     } else if (command == "read") {
-      read_file(option);
+      readFile(arg, address_list);
     } else if (command == "list") {
+      address_list.printList();
     } else if (command == "find") {
+      address_list.findAndPrint(arg);
     } else if (command == "add") {
+      // 중복검사
+      if (address_list.find(arg) != nullptr) {
+        cout << arg << " already exists\n";
+        continue;
+      }
+      // 추가
+      address_list.add(arg);
     } else if (command == "trace") {
     } else if (command == "delete") {
     } else if (command == "save") {
