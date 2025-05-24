b30968629cc59971e9910d838fe70ebb40bcd4af
SH-MooDy <shlee4167@naver.com>
2025-05-24 15:23:36 +0900

    해윙

---
+++ chapter5/prob1.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <unordered_map>s
+using namespace std;
+
+int main() { return 0; }
---
+++ group_activity/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int fun(int X[], k) {
+  n = 7;
+  char freqMap[4];
+}
+int main() {
+  int X[7];
+  int k;
+  for (int i = 0; i < 7; i++) {
+    cin >> X[i];
+  }
+  cin >> k;
+
+  return 0;
+}
